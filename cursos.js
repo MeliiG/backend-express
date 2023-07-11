8c5a07d9fd2c7c65c5cb908a21108741b1bc5c8b
MeliiG <Meli.bogar15@gmail.com>
2023-07-11 09:27:23 -0500

    inicio

---
+++ cursos.js
@@ -0,0 +1,45 @@
+let infoCursos ={
+
+    programacion:[
+        {
+            id: 1,
+            titulo:'conceptos Phyton',
+            lenguaje: 'Phyton',
+            vistas: '15000',
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titulo:'intermedio Phyton',
+            lenguaje: 'Phyton',
+            vistas: '13500',
+            nivel: 'intermedio'
+        },
+        {
+            id: 3,
+            titulo:'Conceptos Javascript',
+            lenguaje: 'Javascript',
+            vistas: '15580',
+            nivel: 'basico'
+        },
+    ],
+
+    Matematicas:[
+        {
+            id: 1,
+            titulo:'Conceptos Calculo',
+            tema: 'Calculo',
+            vistas: '12427',
+            nivel: 'basico'
+        },
+        {
+            id: 2,
+            titulo:'Conceptos Algebra',
+            tema: 'algebra',
+            vistas: '15788',
+            nivel: 'intermedio'
+        },
+    ]
+}
+
+module.exports.infoCursos = infoCursos;
